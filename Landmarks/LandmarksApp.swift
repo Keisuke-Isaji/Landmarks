e0d1a204d6da2be2165ef068eaea0b98d21345ad
Keisuke Isaji <keisuke_isaji@mail.toyota.co.jp>
2024-02-04 16:35:11 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 伊佐治恵介 on 2024/02/04.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
