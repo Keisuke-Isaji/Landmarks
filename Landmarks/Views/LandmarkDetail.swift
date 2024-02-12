d4afc2568e390d4d84c7b826d2c042a4529bf9a0
Keisuke Isaji <keisuke_isaji@mail.toyota.co.jp>
2024-02-13 00:46:08 +0900

    Building lists and navigation終了

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -8,37 +8,51 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+
+
     var body: some View {
-        MapView()
-            .frame(height: 300)
-        CircleImage()
-            .offset(y: -130)
-            .padding(.bottom, -130)
-        VStack(alignment:.leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
+        ScrollView {
+            VStack {
+                MapView(coordinate: landmark.locationCoordinate)
+                    .frame(height: 300)
+
+
+                CircleImage(image: landmark.image)
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+
+
+                VStack(alignment: .leading) {
+                    Text(landmark.name)
+                        .font(.title)
+
+
+                    HStack {
+                        Text(landmark.park)
+                        Spacer()
+                        Text(landmark.state)
+                    }
                     .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+
+
+                    Divider()
+
+
+                    Text("About \(landmark.name)")
+                        .font(.title2)
+                    Text(landmark.description)
+                }
+                .padding()
             }
-            .font(.subheadline)
-            .foregroundStyle(.secondary)
-
-            Divider()
-
-            Text("About Turtle Rock")
-                .font(.title2)
-            Text("Descriptive text goes here")
+            .navigationTitle(landmark.name)
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
-
-        Spacer()
     }
 }
 
+
 #Preview {
-    LandmarkDetail()
+    LandmarkDetail(landmark: landmarks[0])
 }
